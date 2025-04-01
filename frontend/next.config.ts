6ecd0372377b7e6f18b8e516abadfe0cfca2afea
Jihee Hwang <alarmringing@gmail.com>
2025-03-31 23:18:52 -0400

    Added strapi domain as allowlist in image loading

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,17 +1,19 @@
 import type { NextConfig } from "next";
 
+const STRAPI_HOST = process.env.NEXT_PUBLIC_STRAPI_URL || 'http://localhost:1337';
+const url = new URL(STRAPI_HOST);
+
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '1337',
-        pathname: '/uploads/**', // Adjust if your Strapi upload path is different
+        protocol: url.protocol.replace(':', '') as 'http' | 'https',
+        hostname: url.hostname,
+        port: url.port || '',
+        pathname: '/uploads/**',
       },
     ],
   },
-  /* other config options can go here */
 };
 
 export default nextConfig;
